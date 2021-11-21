68a94ac28272e001aa78492a5783903ca3078de8
Diego-Melo <diego.nac@hotmail.com>
2021-11-21 02:09:23 -0300

    Adição de página de erro 404

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ app = Flask(__name__, template_folder='./templates')
 app.secret_key = b'wr\x01\xf4\x84U0\xec\x97\xbaA\xda\xd0^\x97\xab'
 
 # Conexão com o banco de dados
-client = pymongo.MongoClient('localhost',27017)
+client = pymongo.MongoClient('localhost', 27017)
 db = client.login
 
 # Rotas
-from estado import routes
 from user import routes
-
+from estado import routes
 @app.route('/')
 def home():
     if 'logado' in session:
@@ -20,12 +19,20 @@ def home():
     else:
         return render_template('login.html')
 
+
 @app.route('/dashboard')
 def dashboard():
     if 'logado' in session:
         return render_template('dashboard.html')
     else:
-        return redirect('/')
-    
-if (__name__=="__main__"):
+        return redirect('/nao-logado')
+
+# Rotas de erro
+@app.errorhandler(404)
+def naoEncontrado(err):
+    return render_template('erro404.html'), 404
+
+
+if (__name__ == "__main__"):
     app.run(host='0.0.0.0', port=80)
+    
